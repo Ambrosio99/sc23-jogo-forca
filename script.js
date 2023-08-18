f8b221b7d91690fa5a7d905f6144762dc6c17609
Lucas Ambrosio <lu.amb99@gmail.com>
2023-08-18 19:19:33 -0300

    html /css

--- script.js
+++ script.js
@@ -1,37 +1,15 @@
-// Lista de palavras e escolha da palavra transformada em array para inicio do jogo
-const arrayPalavras = [
-  "Escada",
-  "Futebol",
-  "Fralda",
-  "Gato",
-  "Tesoura",
-  "Pular",
-  "Banho",
-  "Piada",
-  "Faca",
-  "Banana",
-  "Chave",
-  "Sapato",
-  "Casa",
-  "Bolsa",
-  "Sorvete",
-  "Cadeira",
-  "Flores",
-  "Livro",
-  "Bateria",
-  "Carro",
-];
-const palavraEscolhida =
-  arrayPalavras[Math.floor(Math.random() * arrayPalavras.length)];
+// Definição inicial do jogo.
+const arrayPalavras = ["Escada", "Futebol", "Fralda", "Gato", "Tesoura", "Pular", "Banho", "Piada", "Faca", "Banana", "Chave", "Sapato", "Casa", "Bolsa", "Sorvete", "Cadeira", "Flores", "Livro", "Bateria", "Carro"];
+const palavraEscolhida = arrayPalavras[Math.floor(Math.random() * arrayPalavras.length)];
 const palavraArray = palavraEscolhida.toUpperCase().split("");
-
-// Puxando pra dentro do html o input que sera passado as letras e <ul> onde vão ser colocadas as letras em formato de <li>
-const letraInput = document.getElementById("inputLetra");
-const listaPalavra = document.getElementById("lista-palavra");
-
-// Array para guardar quando a letra estiver certa e errada
 let letrasErradas = [];
 let letrasCorretas = [];
+
+// Puxando pra dentro do javascript o input que sera passado as letras e <ul> onde vão ser colocadas as letras em formato de <li>
+const letraInput = document.getElementById("inputLetra");
+const listaPalavra = document.getElementById("lista-palavra");
+const buttonLetra = document.getElementById("inputButton");
+const resetGame = document.getElementById("reset-game");
 
 // Função para colcocar dentro do html o numero de letras da palavra em "_"
 function palavraToHTML() {
@@ -50,13 +28,6 @@ function trocarLetra(letra, index) {
   liLetra[index].innerHTML = letra;
 }
 
-// Receber letra do html para javascript com Click e Enter
-letraInput.addEventListener("keydown", (event) => {
-  if (event.key === "Enter") {
-    event.preventDefault();
-    letraRecebida();
-  }
-});
 // função para receber a letra que é passada no input html ja com verificação caso a pessoa passe numeros
 function letraRecebida() {
   let letra = letraInput.value.toUpperCase();
@@ -65,12 +36,13 @@ function letraRecebida() {
   } else {
     alert("Digitar somente letras!");
   }
+  letraInput.value = "";
 }
 
 // Função para verificação da letra, se ela ja foi passada, se está certa ou errada e verificação se ja teve 6 erros.
 function verificarLetra(letra) {
   if (letrasCorretas.includes(letra) || letrasErradas.includes(letra)) {
-    alert("Essa letra ja foi");
+    letraJaFoi();
   } else if (palavraArray.includes(letra)) {
     letrasCorretas.push(letra);
     palavraArray.forEach((item, index) => {
@@ -78,9 +50,10 @@ function verificarLetra(letra) {
     });
   } else {
     letrasErradas.push(letra);
+    attSpanErros();
     letrasErradas.length === 6 ? elementPerdeu() : "";
   }
-  verificarGanhar()
+  verificarGanhar();
 }
 
 // Função para verificação se ainda existe algum "_" caso exista retorna true e não faz nada / caso não exista retorna que você ganhou afinal trocou todos por letras.
@@ -93,14 +66,51 @@ function verificarGanhar() {
   seraseGanhou.includes("_") ? "" : elementGanhou();
 }
 
-// Funções para criar elemento dentro do html avisando quando ganhou ou perdeu.
+// Funções para interação diretamente com o DOM
 function elementGanhou() {
-  const h1 = document.createElement("h1")
-  h1.innerText = "Você ganhou";
+  const h1 = document.createElement("h1");
+  h1.classList.add("ganhou");
+  h1.innerText = "Você ganhou!";
   listaPalavra.appendChild(h1);
+  desativarJogo();
 }
+
 function elementPerdeu() {
-  const h1 = document.createElement("h1")
-  h1.innerText = "Você perdeu";
+  const h1 = document.createElement("h1");
+  h1.classList.add("perdeu");
+  h1.innerText = "Você perdeu!";
   listaPalavra.appendChild(h1);
+  desativarJogo();
+}
+
+function desativarJogo() {
+  letraInput.setAttribute("disabled", true);
+  buttonLetra.setAttribute("disabled", true);
+  return `O jogo acabou, aperte o reset ou atualize a pagina!`;
+}
+
+function attSpanErros() {
+  const span = document.getElementById("span-erros");
+  span.innerText = ` ${letrasErradas.length}`;
+  const letras = document.getElementById("letras-erros");
+  letras.innerText = letrasErradas;
+}
+
+function eventsDom() {
+  resetGame.addEventListener("click", () => location.reload());
+  letraInput.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      letraRecebida();
+    }
+  });
+}
+eventsDom();
+
+function letraJaFoi() {
+  const local = document.getElementById("formForca");
+  const p = document.createElement("p");
+  p.classList.add("jaFoi");
+  p.innerText = "Essa letra ja foi!";
+  local.appendChild(p);
 }
